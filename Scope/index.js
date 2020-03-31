1491eb98d171e4503fdde2750a3e816b646ebbd0
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-03-31 20:34:53 +0500

    Update index.js
    updated "Scope" File

--- Scope/index.js
+++ Scope/index.js
@@ -1,46 +1,46 @@
 // ----------------------------
 //  example : 1
+/*
 
-
-var userName = "Ameen ALam";
+var userName = "Ameen ALam";   // Global Scope
 
 function modifyUserName() {
-        userName = "Daniyal Nagori";
+        userName = "Daniyal Nagori"; // Local Scope
     };
 
 function showUserName() {
-        alert(userName);
+        alert(userName);        // Local Scope
     };
 
-alert(userName); // display Ameen Alam
+alert(userName); // display Ameen Alam          // Display Global Variable
 
 modifyUserName();
-showUserName();// display Daniyal Nagori
+showUserName();// display Daniyal Nagori        // Display Local Variable
 
-
+*/
 
 // --------------------------
 //  example : 2
 
 /* 
 function createUserName() {
-    userName = "Ameen Alam";
+    userName = "Ameen Alam";  // Local Scope
 }
 
 function modifyUserName() {
     if(userName)
-        userName = "Daniyal Nagori";
+        userName = "Daniyal Nagori";    // Local Scope
 };
 
 function showUserName() {
-    alert(userName);  
+    alert(userName);            // Local Scope
 }
 
 createUserName();
-showUserName(); // Ameen Alam 
+showUserName(); // Ameen Alam   // Display Local Variable
 
 modifyUserName();
-showUserName(); // Daniyal Nagori 
+showUserName(); // Daniyal Nagori // Display Local Variable
 
  */
 
@@ -52,15 +52,15 @@ showUserName(); // Daniyal Nagori
 /* 
 
 function createUserName() {
-    var userName = "Ameen Alam";
+    var userName = "Ameen Alam";        // Local Scope
 }
 
 function showUserName() {
-    alert(userName);
+    alert(userName);            // Local Scope
 }
 
-createUserName();
-showUserName(); // throws error: userName is not defined
+createUserName();               // Display Local Variable
+showUserName(); // throws error: userName is not defined      // Can't Display Local Scope Variable of another Function
 
  */
 
@@ -73,18 +73,18 @@ showUserName(); // throws error: userName is not defined
 
 
 /* 
-var userName = "Ameen Alam";
+var userName = "Ameen Alam";            // Global Scope
 
 function ShowUserName()
 {
-    var userName = "Daniyal Nagori";
+    var userName = "Daniyal Nagori";    // Local Scope
 
-    alert(userName); // "Daniyal Nagori"
+    alert(userName); // "Daniyal Nagori"        // // Display Local Variable
 }
 
 ShowUserName();
 
-alert(userName); // Ameen Alam
+alert(userName); // Ameen Alam          // Display Global Variable
 
  */
 
@@ -99,14 +99,14 @@ function NoBlockLevelScope(){
     
     if (1 > 0)
     {
-        var myVar = 22;
+        var myVar = 22;         // Block Level Scope
 
     }
 
     alert(myVar);
 }
 
-NoBlockLevelScope();
+NoBlockLevelScope();            // Will Display Block Variable
  */
 
 
@@ -116,15 +116,15 @@ NoBlockLevelScope();
 
 
 /* 
-var age = 100;
+var age = 100;          // Global Scope
 function go(){
- var age = 200;
- var hair =  'black';
+ var age = 200;         // Local Scope
+ var hair =  'black';   // Local Scope
  console.log(age);
  console.log(hair);
 }
-go();
-console.log(age);
+go();                   // Will Display Local Variables
+console.log(age);       // Will Display Global Variable
 
 // OUTPUT  200 
 // OUTPUT  black 
@@ -141,18 +141,18 @@ console.log(age);
 
 if (true) {
    // userName is in the global scope because of the 'var' keyword
-   var userName = 'Ameen Alam';
+   var userName = 'Ameen Alam';                 // Local Scope
    console.log(userName); // output 'Ameen Alam'
    // age is in the local scope because of the 'let' keyword
-   let age = 20;
+   let age = 20;                                // Block Level Scope
    console.log(age); // output 20
    // skills is in the local scope because of the 'const' keyword
-   const skills = 'JavaScript';
+   const skills = 'JavaScript';                 // Block Level Scope
    console.log(skills); // output 'JavaScript'
 }
-console.log(userName); // output 'Ameen Alam'
-console.log(age); // Uncaught ReferenceError: age is not defined
-console.log(skills); // Uncaught ReferenceError: skills is not defined
+console.log(userName); // output 'Ameen Alam'   // Will display Local Variable
+console.log(age); // Uncaught ReferenceError: age is not defined        // Can't display block level variable
+console.log(skills); // Uncaught ReferenceError: skills is not defined  // Can't display block level variable
 
 
 */
